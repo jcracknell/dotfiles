97f306653c2da0a1de52cae4382c2c14e3032a97
James Cracknell <james_cracknell@gov.nt.ca>
2012-08-14 08:59:39 -0600

    Added vimperatorrc

--- install.bat
+++ install.bat
@@ -2,3 +2,4 @@
 set DOTFILES=%~dp0
 mklink /D "%USERPROFILE%\vimfiles" "%DOTFILES%vim"
 mklink "%USERPROFILE%\.vimrc" "%DOTFILES%vim\vimrc"
+mklink "%USERPROFILE%\.vimperatorrc" "%DOTFILES%vimperatorrc"
--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 DOTFILES="$(pwd)/$(dirname "$0")"
 ln -s "$DOTFILES/vim" "$HOME/.vim"
 ln -s "$DOTFILES/vim/vimrc" "$HOME/.vimrc"
+ln -s "$DOTFILES/vimperatorrc" "$HOME/.vimperatorrc"
