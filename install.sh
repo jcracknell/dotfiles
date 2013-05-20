874822a329043aea1f8334285355859380dd300f
James Cracknell <james.h.cracknell@gmail.com>
2013-05-20 11:47:10 -0600

    Add sbt configuration

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DOTFILES="$(pwd)/$(dirname "$0")"
+ln -s "$DOTFILES/sbt" "$HOME/.sbt"
 ln -s "$DOTFILES/vim" "$HOME/.vim"
 ln -s "$DOTFILES/vim/vimrc" "$HOME/.vimrc"
 ln -s "$DOTFILES/vimperatorrc" "$HOME/.vimperatorrc"
